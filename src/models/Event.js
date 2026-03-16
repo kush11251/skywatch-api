57cb7671fd79adef0640a331ef7295901d6516c0
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-17 03:36:41 +0530

    feat: integrate src/models/Event.js

---
+++ src/models/Event.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const eventSchema = new mongoose.Schema({
+    name: String,
+    date: Date,
+    description: String
+});
+
+const Event = mongoose.model('Event', eventSchema);
+module.exports = Event;
